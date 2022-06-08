bb455630f9f1c5d9b27e314e5f80b30c365cb4e0
Supitcha Jampathong (NTL) <CF303956@tidlor.com>
2022-06-08 17:01:59 +0700

    init myapp

---
+++ Hw2/Doctor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw2
+{
+    public class Doctor
+    {
+        public string Name { get; set; }
+        public string Occuration { get; set; }
+        private decimal Salary { get; set; }
+        public decimal Tax { get; set; }
+
+        public Doctor(string name, string occuration, decimal salary, decimal tax)
+        {
+            this.Name = name;
+            this.Occuration = occuration;
+            this.Salary = salary;
+            this.Tax = tax;
+        }
+
+        public string WhoIam()
+        {
+            var result = "";
+            result = $"{Name} is a {Occuration}";
+            //result = this.Name +" is a "+this.Occuration;
+            return result;
+
+        }
+
+        public decimal GetTax()
+        {
+            return (Salary * Tax) / 100;
+        }
+
+       
+
+
+
+
+    }
+}
---
+++ Hw2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Hw2;
+
+Console.Write("Enter Your Name : ");
+var name = Console.ReadLine();
+
+Console.Write("Enter Your Occuration : ");
+var occuration = Console.ReadLine();
+
+Console.Write("Enter Your Salary : ");
+var salary = Console.ReadLine();
+
+Console.Write("Enter Your Tax : ");
+var tax = Console.ReadLine();
+
+
+var changesalary = decimal.Parse(salary);
+var changetax = decimal.Parse(tax);
+
+
+var callJob = new Doctor(name, occuration, changesalary, changetax);
+var whoname = callJob.WhoIam();
+var pay = callJob.GetTax();
+Console.WriteLine($"{whoname}\n");
+Console.WriteLine($"{pay}\n");
+
+
+
+
